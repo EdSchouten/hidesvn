27719884a9ed310ac325e728d0bc9a631641431f
Ed Schouten <ed@80386.nl>
2009-02-24 20:17:58 +0100

    Don't forget to mention Jille.

--- libhidesvn.c
+++ libhidesvn.c
@@ -1,6 +1,9 @@
 /*-
  * Copyright (c) 2009 Ed Schouten <ed@80386.nl>
  * All rights reserved.
+
+ * Copyright (c) 2009 Jille Timmermans <jille@quis.cx>
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
